607684b4494c3ad14e8dd02d0ee2b65e293d1863
Harvey <97197014+YerikAH@users.noreply.github.com>
2025-09-15 15:30:46 -0500

    feat(reservation): :sparkles: Mejorar la función de listado de reservas. Se añade soporte para los estados de reserva 'confirmado' y 'cancelado', y se implementa la búsqueda asíncrona de reservas. Se incluyen registros de consola para depuración.

--- src/modules/reservation/features/list_my_reservations/data/list_my_reservations.repository.ts
+++ src/modules/reservation/features/list_my_reservations/data/list_my_reservations.repository.ts
@@ -2,12 +2,19 @@ import prisma from "../../../../../config/prisma_client";
 import { reservation_status } from "@prisma/client";
 
 export class ListMyReservationsRepository {
-  list(user_id: string, skip: number, take: number) {
-    return prisma.reservation.findMany({
+  async list(user_id: string, skip: number, take: number) {
+    console.log(user_id);
+
+    const data = await prisma.reservation.findMany({
       where: {
         user_id,
         status: {
-          in: [reservation_status.pending, reservation_status.in_progress],
+          in: [
+            reservation_status.pending,
+            reservation_status.in_progress,
+            reservation_status.confirmed,
+            reservation_status.cancelled,
+          ],
         },
       },
       orderBy: { start_time: "desc" },
@@ -22,5 +29,7 @@ export class ListMyReservationsRepository {
       skip,
       take,
     });
+    console.log(data);
+    return data;
   }
 }
